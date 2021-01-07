fda1ddd8a0d198048191b0209a7d5577fdd308d5
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2021-01-07 19:26:55 +0100

    Update projekt.sql

--- projekt.sql
+++ projekt.sql
@@ -1,4 +1,4 @@
-
+drop database projekt;
 CREATE DATABASE projekt;
 USE projekt;
 
@@ -74,64 +74,58 @@ CREATE TABLE stadion(
 );
 CREATE TABLE sesija(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tim1_id INT NOT NULL,
-    tim2_id INT NOT NULL,
+    id_tim1 INT NOT NULL,
+    id_tim2 INT NOT NULL,
+    id_sudac INT NOT NULL,
+    id_stadion INT NOT NULL,
     datum_sesija DATETIME,
-    CONSTRAINT CHK_tim2 CHECK ( tim1_id != tim2_id ),
-    FOREIGN KEY (tim1_id) REFERENCES tim(id),
-    FOREIGN KEY (tim1_id) REFERENCES tim(id)
-);
-CREATE TABLE sudac_sesija(
-	id_sesija INT NOT NULL,
-	id_sudac INT NOT NULL,
-    tip VARCHAR(25),
-    FOREIGN KEY (id_sesija) REFERENCES sesija(id),
-    FOREIGN KEY (id_sudac) REFERENCES sudac(id)
+    CONSTRAINT CHK_tim2 CHECK ( id_tim1 != id_tim2 ),
+    FOREIGN KEY (id_tim1) REFERENCES tim(id),
+    FOREIGN KEY (id_tim2) REFERENCES tim(id),
+    FOREIGN KEY (id_sudac) REFERENCES sudac(id),
+    FOREIGN KEY (id_stadion) REFERENCES stadion(id)
 );
 
-CREATE TABLE statistika_opcenito(
+CREATE TABLE out_s(
 	id_sesija INT NOT NULL,
-	id_tim1 INT NOT NULL,
+    id_tim1 INT NOT NULL,
     id_tim2 INT NOT NULL,
-    golovi_tim1 INT NOT NULL,
-    golovi_tim2 INT NOT NULL,
-    posjed_lopte_tim1 INT NOT NULL,
-    posjed_lopte_tim2 INT NOT NULL,
-    out_tim1 INT NOT NULL,
-    out_tim2 INT NOT NULL,
-    sutevi_ukupno_tim1 INT NOT NULL,
-    sutevi_ukupno_tim2 INT NOT NULL,
-    sutevi_okvir_tim1 INT NOT NULL,
-    sutevi_okvir_tim2 INT NOT NULL,
-	FOREIGN KEY (id_sesija) REFERENCES sesija(id),
+    broj_outova INT DEFAULT 0,
+    CONSTRAINT CHK_tim12 CHECK ( id_tim1 != id_tim2 ),
     FOREIGN KEY (id_tim1) REFERENCES tim(id),
     FOREIGN KEY (id_tim2) REFERENCES tim(id)
+    
 );
 
-CREATE TABLE statistika_kazne(
+CREATE TABLE gol(
 	id_sesija INT NOT NULL,
-	id_igrac INT NOT NULL,
-	id_tim INT NOT NULL,
-    id_sudac INT NOT NULL,
-    tip_kazne VARCHAR(25),
-    vrijeme_kazne TIME,
-    FOREIGN KEY (id_tim) REFERENCES tim(id),
-    FOREIGN KEY (id_sesija) REFERENCES sesija(id),
-    FOREIGN KEY (id_igrac) REFERENCES igrac(id),
-    FOREIGN KEY (id_sudac) REFERENCES sudac(id)
-);
-
-CREATE TABLE statistika_zamjene(
-	id_sesija INT NOT NULL,
-    id_igrac1 INT NOT NULL,
-    id_igrac2 INT NOT NULL,
     id_tim INT NOT NULL,
-    vrijeme_zamjene TIME,
+    id_igrac INT NOT NULL,
     FOREIGN KEY (id_sesija) REFERENCES sesija(id),
-    FOREIGN KEY (id_igrac1) REFERENCES igrac(id),
-    FOREIGN KEY (id_igrac2) REFERENCES igrac(id),
+    FOREIGN KEY (id_tim) REFERENCES tim(id),
+    FOREIGN KEY (id_igrac) REFERENCES igrac(id)
+);
+
+CREATE TABLE kazne(
+	id_sesija INT NOT NULL,
+    id_tim INT NOT NULL,
+    id_igrac INT NOT NULL,
+    tip_kazne VARCHAR(20),
+    FOREIGN KEY (id_sesija) REFERENCES sesija(id),
+    FOREIGN KEY (id_tim) REFERENCES tim(id),
+    FOREIGN KEY (id_igrac) REFERENCES igrac(id)
+);
+
+CREATE TABLE udarci(
+	id_sesija INT NOT NULL,
+    id_tim INT NOT NULL,
+    ukupno INT DEFAULT 0,
+    u_okvir INT DEFAULT 0,
+    FOREIGN KEY (id_sesija) REFERENCES sesija(id),
     FOREIGN KEY (id_tim) REFERENCES tim(id)
 );
+
+
 
 INSERT INTO drzava(ime) 
 VALUES
@@ -242,7 +236,7 @@ SELECT i.id, i.ime, i.prezime, t.ime as tim, d.ime as drzava, g.ime as grad, tr.
  LEFT JOIN drzava as d ON i.id_drzava=d.id
  LEFT JOIN grad as g ON i.id_grad=g.id
  LEFT JOIN trener as tr ON tr.id_tim=t.id
- WHERE i.id_tim=1
+ WHERE i.id_tim=1 /* umjesto 1 vanjska varijabla X */
  GROUP BY i.id;
  
  drop function br_s;
@@ -253,9 +247,9 @@ DETERMINISTIC
 	DECLARE rez INT;
     
     SELECT COUNT(*) as broj_sudjenja INTO rez
- FROM sudac AS s LEFT JOIN sudac_sesija AS se
+ FROM sudac AS s LEFT JOIN sesija AS se
  ON s.id=se.id_sudac WHERE s.id=p_id_sudac;
     RETURN rez;
 END //
 DELIMITER ;
-SELECT br_s(1);
+SELECT br_s(4);
